d63e291e281c28be0640641bd139ba4de0e03a0c
Felix Anducho <felix.anducho74@gmail.com>
2022-01-08 13:46:07 -0600

    Split code from HeroRoles to util function

--- src/components/HeroRoles.vue
+++ src/components/HeroRoles.vue
@@ -7,30 +7,13 @@
 </template>
 
 <script>
-const CarryIcon = require('../assets/img/Carry.png')
-const DisablerIcon = require('../assets/img/Disabler.png')
-const DurableIcon = require('../assets/img/Durable.png')
-const EscapeIcon = require('../assets/img/Escape.png')
-const InitiatorIcon = require('../assets/img/Initiator.png')
-const JunglerIcon = require('../assets/img/Jungler.png')
-const NukerIcon = require('../assets/img/Nuker.png')
-const PusherIcon = require('../assets/img/Pusher.png')
-const SupportIcon = require('../assets/img/Support.png')
+import { ROLES } from '../utils/heroIcons'
+
 export default {
     name: 'HeroRoles',
     data() {
         return {
-            ROLES : {
-            Carry: CarryIcon,
-            Disabler: DisablerIcon,
-            Durable : DurableIcon,
-            Escape: EscapeIcon,
-            Initiator : InitiatorIcon,
-            Jungler : JunglerIcon,
-            Nuker : NukerIcon,
-            Pusher : PusherIcon,
-            Support : SupportIcon
-            }
+            ROLES
         }
     },
     props: {
---
+++ src/utils/heroIcons.js
@@ -0,0 +1,21 @@
+const CarryIcon = require("../assets/img/Carry.png");
+const DisablerIcon = require("../assets/img/Disabler.png");
+const DurableIcon = require("../assets/img/Durable.png");
+const EscapeIcon = require("../assets/img/Escape.png");
+const InitiatorIcon = require("../assets/img/Initiator.png");
+const JunglerIcon = require("../assets/img/Jungler.png");
+const NukerIcon = require("../assets/img/Nuker.png");
+const PusherIcon = require("../assets/img/Pusher.png");
+const SupportIcon = require("../assets/img/Support.png");
+
+export const ROLES = {
+  Carry: CarryIcon,
+  Disabler: DisablerIcon,
+  Durable: DurableIcon,
+  Escape: EscapeIcon,
+  Initiator: InitiatorIcon,
+  Jungler: JunglerIcon,
+  Nuker: NukerIcon,
+  Pusher: PusherIcon,
+  Support: SupportIcon,
+};
