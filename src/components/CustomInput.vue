4a5e3fd87ec02740124b7e2e694fa959bc158d34
Felix Anducho <felix.anducho74@gmail.com>
2022-01-10 23:02:26 -0600

    Added CustomInput component

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <label for="name">{{label}}</label>
+      <input 
+        type="text"
+        :value="modelValue"
+        :placeholder="placeholder"
+        @input="(e) => $emit('update:modelValue', e.target.value)"
+      >
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        label: String,
+        placeholder: String,
+        modelValue: String
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div>
-      <h1>{{ title }} </h1>
+      <h1>{{ title }}</h1>
+      <CustomInput
+      v-model="heroName"
+      :placeholder="'Hero name to search...'"
+      :label="'Hero Search:'"
+      />
+      <button>Search</button>
       <HeroesGrid/>
   </div>
 </template>
 
 <script>
 import HeroesGrid from './HeroesGrid.vue'
+import CustomInput from './CustomInput.vue'
 export default {
-  components: { HeroesGrid },
+  components: { HeroesGrid, CustomInput },
+  data() {
+      return {
+          heroName: ''
+      }
+  },
   props: {
       title: String
   }
-
 }
 </script>
 
